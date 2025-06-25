081a99d8d44c0a18724b2da2d3da36c91e9ef7c0
Siva <sivalayavinod25@gmail.com>
2025-06-25 09:53:07 +0530

    update: Update input checked property

--- src/components/TodoListView.vue
+++ src/components/TodoListView.vue
@@ -1,7 +1,7 @@
 <template>
   <ul v-for="todo in storeCounter.todoList" :key="todo.id" class="todo-list">
     <li>
-      <input type="checkbox" />
+      <input type="checkbox" v-bind:checked="todo.completed" />
       <span>{{ todo.title }}</span>
     </li>
   </ul>
@@ -13,7 +13,7 @@ import { useCounterStore } from '@/stores/counter'
 const storeCounter = useCounterStore()
 
 const props = defineProps<{
-  checked: boolean
+  checked?: boolean
 }>()
 
 const emits = defineEmits<{
@@ -32,16 +32,3 @@ const emits = defineEmits<{
   }
 }
 </style>
-
-<!-- Typing      : [47wpm] [99%]
-Focus       : [11hr 10min] [1642hr 29min]
-CT          : [10hr 52min] [1545hr 59min]
-ACT         : [09hr 08min] [1382hr 49min]
-HTML        : [00] [4741]
-CSS         : [00] [17564]
-JS          : [00] [13378]
-TS          : [20] [67]
-Vue         : [131] [2810]
-JSON        : [00] [46]
-Total       : [151] [38460]
-Day         : 17 -->
